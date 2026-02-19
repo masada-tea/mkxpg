90def7cff904494250a7b3773624b907197ff121
masada-tea <64900337+masada-tea@users.noreply.github.com>
2026-02-19 23:54:41 +0900

    build_config.rb を更新

--- extra/build_config.rb
+++ extra/build_config.rb
@@ -8,9 +8,6 @@ MRuby::CrossBuild.new('wasm32-unknown-gnu') do |conf|
   
     conf.gembox 'default'
     
-    # mruby-onig-regexpを無効化
-    conf.disable_gem 'mruby-onig-regexp' if conf.respond_to?(:disable_gem)
-    
     conf.gem :github => 'pulsejet/mruby-marshal'
     conf.gem :github => 'monochromegane/mruby-time-strftime'
     conf.gem :core => 'mruby-eval'
